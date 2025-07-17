f0f762bf528ae8907818be6c32c8d0ed5e808170
maltinikova <maltinikova@inventive.ru>
2025-07-17 13:29:53 +0300

    comit

---
+++ assets/js/auth.js
@@ -0,0 +1,135 @@
+$(document).ready(function(){
+
+    let inProgress = false;
+
+    $('.js-auth').on('click', function(){
+        if(inProgress) return;
+        inProgress = true;
+        hideError();
+        let formContainer = $(this).parents('form');
+
+        $.ajax({
+            type: "POST",
+            data: formContainer.serialize(),
+            url: '/api/user/login',
+            success: function (result) {
+                if(result.success) {
+                    window.location.href = result.params["redirect"];
+                }
+                else {
+                    formContainer.find('input:not(.checkbox-input)').parent().addClass('form-error');
+                    formContainer.find('input:not(.checkbox-input)').parent().find('.js-error').text(result.error);
+                }
+                inProgress = false;
+            }
+        });
+    });
+
+    $('.js-register').on('click', function(){
+        if(inProgress) return;
+        inProgress = true;
+        hideError();
+        let form = $(this).parents('form');
+
+        $.ajax({
+            type: "POST",
+            data: $(this).parents('form').serialize(),
+            url: '/api/user/register',
+            success: function (result) {
+                if(result.success) {
+                    window.location.href = result.params["redirect"];
+                }
+                else {
+                    $.each(result.params["errors"], function(index, value) {
+                        let input = form.find('input[name="registration_form['+index+']"]');
+                        if (input.length === 0) {
+                            input = form.find('input[name="registration_form[password][' + index + ']"]');
+                        }
+
+                        if(input.length > 0) {
+                            input.parent().addClass('form-error').find('.js-error').text(value);
+                        }
+                    });
+                }
+                inProgress = false;
+            }
+        });
+    });
+
+    $('.js-send-verify').on('click', function(){
+        if(inProgress) return;
+        inProgress = true;
+        hideError();
+
+        $.ajax({
+            type: "POST",
+            data: {},
+            url: '/api/user/send_verify',
+            success: function (result) {
+                if(result.success) {
+                    window.location.href = result.params["redirect"];
+                }
+                else {
+                }
+                inProgress = false;
+            }
+        });
+    });
+
+    $('.js-forgot').on('click', function(){
+        if(inProgress) return;
+        inProgress = true;
+        hideError();
+        let formContainer = $(this).parents('form');
+
+        $.ajax({
+            type: "POST",
+            data: formContainer.serialize(),
+            url: '/api/user/forgot',
+            success: function (result) {
+                if(result.success) {
+                    window.location.href = result.params["redirect"];
+                }
+                else {
+                    $.each(result.params["errors"], function(index, value) {
+                        let input = formContainer.find('input[name="reset_password_request_form['+index+']"]');
+                        if(input.length > 0) {
+                            input.parent().addClass('form-error').find('.js-error').text(value);
+                        }
+                    });
+                }
+                inProgress = false;
+            }
+        });
+    });
+
+    $('.js-change').on('click', function(){
+        if(inProgress) return;
+        inProgress = true;
+        hideError();
+        let formContainer = $(this).parents('form');
+        let successContainer = $('.success-block');
+
+        $.ajax({
+            type: "POST",
+            data: $(this).parents('form').serialize(),
+            url: '/api/user/reset',
+            success: function (result) {
+                if(result.success) {
+                    formContainer.parents('.page_window').remove();
+                    successContainer.show();
+                }
+                else {
+                    $.each(result.params["errors"], function(index, value) {
+                        input = formContainer.find('input[name="change_password_form[plainPassword][' + index + ']"]');
+                        if(input.length > 0) {
+                            input.parent().addClass('form-error').find('.js-error').text(value);
+                        }
+                    });
+                }
+                inProgress = false;
+            }
+        });
+    });
+
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -31,7 +31,78 @@ $(document).ready(function() {
 
     });
 
+    let burgerBtn = $('.js-burger');
+    let mainMenu = $('.js-main-menu');
+    burgerBtn.on('click',function(){
+        $('body').addClass('modal');
+    });
 
-    // Устанавливаем первый активный слайд при загрузке страницы
-    goToSlide(0);
+    let modalCloseBtn = $('.js-modal-close');
+    modalCloseBtn.on('click',function(){
+        $('body').removeClass('modal');
+    });
+
+    $(document).on('keydown','textarea, input',function(){
+        hideError();
+    });
+
+    $(document).on('change','input[type="checkbox"]',function(){
+        hideError();
+    });
+
+    $( window ).on( "resize", function() {
+        $('body').removeClass('modal');
+    });
+
+    $("#demo_download_button").on("click", function() {
+        $.ajax({
+            type: "GET",
+            url: '/api/download/SPARTA2035_DEMO',
+            success: function (result) {
+            }
+        });
+    });
+    $("#full_download_button").on("click", function () {
+        $.ajax({
+            type: "GET",
+            url: '/api/download/SPARTA2035',
+            success: function (result) {
+            }
+        });
+    });
+
+    $('.js-password-toggle').on('click', function(){
+        $(this).toggleClass('active');
+        let input = $(this).parent().find('input');
+        if(input.attr('type') === 'password'){
+            input.attr('type', 'text');
+        }
+        else {
+            input.attr('type', 'password');
+        }
+    });
+
+    $('textarea, input[type="text"], input[type="email"], input[type="password"]').keypress(function (e) {
+        if (!$(this).hascClass('.js-no-submit') && e.which == 13) {
+            $(this).parents('form').find('button').click();
+            return false;
+        }
+    });
+
+    $('.game_link').on('click',function (){
+        $(this).find('a')[0].click();
+    });
+
+    $('.js-cookie-accept').on('click', function(){
+        $.ajax({
+            type: "POST",
+            url: '/api/user/cookie_accept',
+            success: function (result) {
+                $('.cookie_modal').remove();
+            }
+        });
+    })
 });
+function hideError() {
+    $('.form-row').removeClass('form-error');
+}
